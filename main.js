dc7f1eba6099580bc59a71fd6dfa3545e894e81c
Paras-C <paras_c@hotmail.co.uk>
2017-09-13 16:37:32 +0100

    Added Game Squares

---
+++ main.js
@@ -0,0 +1,4 @@
+//get a grid on the screen
+//when you click on each grid tile, make x and then an o appear
+//after each player has made a move, check to see if someone has won
+//if winner, display winner message.
