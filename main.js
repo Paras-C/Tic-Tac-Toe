c7da32f62c932e075dac1a3485e0fa56664fb270
Paras-C <paras_c@hotmail.co.uk>
2017-09-13 17:09:23 +0100

    Added alternating clicks for squares

--- main.js
+++ main.js
@@ -1,4 +1,36 @@
 //get a grid on the screen
+
 //when you click on each grid tile, make x and then an o appear
+var p1 = true;
+var p2 = false;
+  $(".box").one("click",function(event){
+      if(p1==true&&p2==false)
+      {
+        $(this).css("background-color","blue");
+        p1 = false;
+        p2 = true;
+      }
+      else if(p2==true&&p1==false)
+      {
+        $(this).css("background-color","green");
+        p1 = true;
+        p2 = false;
+      }  
+  });
 //after each player has made a move, check to see if someone has won
+
+var winScenarios=[
+[$("b1"),$("b2"),$("b3")],//across top
+[$("b4"),$("b5"),$("b6")],//across middle
+[$("b7"),$("b8"),$("b9")],//across bottom
+[$(""),$(""),$("")],
+[$(""),$(""),$("")],
+[$(""),$(""),$("")],
+[$(""),$(""),$("")],
+[$(""),$(""),$("")],
+
+]
+
+
+
 //if winner, display winner message.
